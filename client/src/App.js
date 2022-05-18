f17d1e12907a5843eba42358498778249b0a60a0
tahrer007 <tahrer_Ad@outlook.com>
2022-05-18 22:59:17 +0300

    app js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
-function App() {
-  return <div className="App">test</div>;
-}
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return <div>test</div>;
+};
 
 export default App;
