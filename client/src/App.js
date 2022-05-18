d9139a63ee4196996712ac246d2777a33837d0d1
tahrer007 <tahrer_Ad@outlook.com>
2022-05-18 23:09:39 +0300

    app router

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,25 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ContactPage from "./pages/ContactPage";
+import ProjectsPage from "./pages/ProjectsPage";
+import AboutPage from "./pages/AboutPage";
+//import ErrorPage from "./pages/errorPage/ErrorPage";
 
 const App = () => {
-  return <div>test</div>;
+  return (
+    <div>
+  <BrowserRouter>
+   
+ <Routes>
+   <Route path="/" element={<HomePage />} />
+   <Route path="/contactme" element={<ContactPage />} />
+   <Route path="/about" element={<AboutPage />} />
+   <Route path="/project" element={<ProjectsPage />} />
+   <Route path="*" element={<ErrorPage />} />
+ </Routes>
+</BrowserRouter>
+</div>);
 };
 
 export default App;
