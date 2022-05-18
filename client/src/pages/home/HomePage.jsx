12f69c312383d4f5a1c5926456d66a71d29cbf94
tahrer007 <tahrer_Ad@outlook.com>
2022-05-18 23:50:33 +0300

    basic home page

--- client/src/pages/home/HomePage.jsx
+++ client/src/pages/home/HomePage.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import "../../app.css"
+import "./home.css"
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const handleClick =(e)=>{
+    
+    //e.preventDefault();
+  }
+  return (<div className="page homePage">
+    <div> Hello, I'm Matthew Williams.
+I'm a full-stack web developer.
+
+
+ </div>
+ <button onClick={handleClick}> view my work</button>
+    
+  
+  </div>);
 };
 
 export default HomePage;
