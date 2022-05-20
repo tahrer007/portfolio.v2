fd733aaf6cfee8ff725e1c5d3c74d9c58020c4b4
tahrer007 <tahrer_Ad@outlook.com>
2022-05-20 18:12:36 +0300

    navbar linkes

--- client/src/components/navIcon/NavBar.jsx
+++ client/src/components/navIcon/NavBar.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <header>
+      <Link className="navButton" to="/">
+        Home
+      </Link>
+      <Link className="navButton" to="/about">
+        About
+      </Link>
+      <Link className="navButton" to="/projects">
+        projects
+      </Link>
+      <Link className="navButton" to="/contactme">
+        contact
+      </Link>
+      
+    </header>
+  );
 };
 
 export default NavBar;
