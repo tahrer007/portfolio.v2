f4903233e27870394c114e20f91ddd767c7595bd
tahrer007 <tahrer_Ad@outlook.com>
2022-05-20 23:31:00 +0300

    .

--- client/src/components/navIcon/NavBar.jsx
+++ client/src/components/navIcon/NavBar.jsx
@@ -2,7 +2,7 @@ import React  ,{useState} from "react";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
-  const [showNav,setShowNav]=useState(false);
+  //const [showNav,setShowNav]=useState(false);
 
   return (
     <header className="">
