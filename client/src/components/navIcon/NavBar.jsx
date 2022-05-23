7972ae7a19fd2823182ce8066698b394251dc31c
tahrer007 <tahrer_Ad@outlook.com>
2022-05-23 21:13:19 +0300

    nav bar  postion

--- client/src/components/navIcon/NavBar.jsx
+++ client/src/components/navIcon/NavBar.jsx
@@ -1,33 +1,28 @@
-import React  ,{useState} from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import "./nav.css"
 
 const NavBar = () => {
   //const [showNav,setShowNav]=useState(false);
 
   return (
-    <header className="navContainer">
-      <div className="navIcon">
-        icon
-      </div>
+    <div className="navContainer">
+      <div className="navIcon">icon</div>
       <div className="navMenu">
-
-      <Link className="navItem" to="/">
-        Home
-      </Link>
-      <Link className="navItem" to="/about">
-        About
-      </Link>
-      <Link className="navItem" to="/projects">
-        projects
-      </Link>
-      <Link className="navItem" to="/contactme">
-        contact
-      </Link>
-
-      </div>
-     
-      
-    </header>
+        <Link className="navItem" to="/">
+          Home
+        </Link>
+        <Link className="navItem" to="/about">
+          About
+        </Link>
+        <Link className="navItem" to="/projects">
+          projects
+        </Link>
+        <Link className="navItem" to="/contactme">
+          contact
+        </Link>
+  </div>
+    </div>
   );
 };
 
