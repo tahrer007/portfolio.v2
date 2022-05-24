a8bdff9198592ddb31526b21ed29407f8fe0e5b2
tahrer007 <tahrer_Ad@outlook.com>
2022-05-24 14:22:43 +0300

    sub containers dimentions

--- client/src/pages/about/AboutPage.jsx
+++ client/src/pages/about/AboutPage.jsx
@@ -10,7 +10,7 @@ const AboutPage = () => {
      <div className="title"> <h1>about</h1> </div> 
       <div className="skillsIconsContainer"> skills icons  </div>
       <div className="detailsContainer"> 
-          personal data and chart 
+         
           <div className="personalDetails">personal details </div>
           <div className="skillsChart">charts </div>
 
