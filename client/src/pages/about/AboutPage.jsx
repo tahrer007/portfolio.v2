993a4c5ba1b45f3d9122528bf8f66ca45689fdca
tahrer007 <tahrer_Ad@outlook.com>
2022-05-24 14:15:28 +0300

    basic divs about pages

--- client/src/pages/about/AboutPage.jsx
+++ client/src/pages/about/AboutPage.jsx
@@ -1,11 +1,20 @@
 import React from "react";
-import { forwardRef } from "react";
+import "../../app.css" ;
+import "./about.css";
+
 
 
 const AboutPage = () => {
   return (
-    <div>
-      <h1>about</h1>
+    <div className="pages aboutPage">
+     <div className="title"> <h1>about</h1> </div> 
+      <div className="skillsIconsContainer"> skills icons  </div>
+      <div className="detailsContainer"> 
+          personal data and chart 
+          <div className="personalDetails">personal details </div>
+          <div className="skillsChart">charts </div>
+
+      </div>
     </div>
   );
 };
