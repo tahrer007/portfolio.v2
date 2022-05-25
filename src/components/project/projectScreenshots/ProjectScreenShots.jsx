e713c8a78882d5a33ed5f3ba9f93a18147be614b
tahrer007 <tahrer_Ad@outlook.com>
2022-05-25 21:15:12 +0300

    preoject page refactor component

---
+++ src/components/project/projectScreenshots/ProjectScreenShots.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./projectScreenShots.css"
+
+
+const ProjectScreenShots = ({project}) => {
+ 
+  return <div className="screenShotsWraper">iamges</div>;
+};
+
+export default ProjectScreenShots;
--- src/pages/projects/ProjectsPage.jsx
+++ src/pages/projects/ProjectsPage.jsx
@@ -1,25 +1,37 @@
 import React from "react";
 import "./projects.css";
+import ProjectDetails from "../../components/project/projectDetails/ProjectDetails";
+import ProjectScreenShots from "../../components/project/projectScreenshots/ProjectScreenShots";
 import { technologiesIcons, projectsData } from "../../data/projectsData";
 
+ /*<div className="projectContainer" >
+            <div className="projectDetailsBox">text</div>
+            <div className="projectScreenShoots">iamges</div>
+    </div>*/
+
 const ProjectsPage = () => {
+ 
   return (
     <div className="pages projectsPage">
       {projectsData.map((project) => {
         return (
-          <div className="projectContainer" key={project.id}style={{ flexDirection: (project.id%2) ? 'row-reverse' : 'row' }}>
-            <div className="projectDetailsBox">text</div>
-            <div className="projectScreenShoots">iamges</div>
+          <div
+            className="projectContainer"
+            key={project.id}
+           ///// style={{ flexDirection: project.id % 2 ? "row-reverse" : "row" }}
+          >
+             <ProjectScreenShots project={project} />
+            <ProjectDetails project={project} />
+          
+         
+           
           </div>
         );
-
       })}
-     
-     {/*<div className="projectContainer" >
-            <div className="projectDetailsBox">text</div>
-            <div className="projectScreenShoots">iamges</div>
-    </div>*/}
+
+   
     </div>
+      
   );
 };
 
