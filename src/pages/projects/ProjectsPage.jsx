d81121de3ff2814d3e5954f1453dd39ac3c328b6
tahrer007 <tahrer_Ad@outlook.com>
2022-05-25 18:19:21 +0300

    project page basic css

--- src/pages/projects/ProjectsPage.jsx
+++ src/pages/projects/ProjectsPage.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import "./projects.css" ;
+import {technologiesIcons,projectsData} from "../../data/projectsData"
 
 const ProjectsPage = () => {
-  return <div>ProjectsPage</div>;
+  return <div className="pages projectsPage">
+    <div className="projectContainer">
+
+    <div className="projectDetailsBox">text</div>
+    <div className="projectScreenShoots">iamges</div>
+    </div>
+    
+
+  </div>;
 };
 
 export default ProjectsPage;
