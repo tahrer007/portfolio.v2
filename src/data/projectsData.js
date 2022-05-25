d1f318637c3e80348cdfe249c44cfb9bfb8abaa9
tahrer007 <tahrer_Ad@outlook.com>
2022-05-25 17:46:01 +0300

    porjects data

---
+++ src/data/projectsData.js
@@ -0,0 +1,82 @@
+const projectsData = [
+  {
+    id: 0,
+    title: "project Title",
+    description:
+      "He lifted the bottle to his lips and took a sip of the drink. He had tasted this before, but he couldn't quite remember the time and place it had happened. He desperately searched his mind trying to locate and remember where he had tasted this when the bicycle ran over his foot.",
+    links: {
+      code: "https://github.com/tahrer007/spot-it-web",
+      website: "https://spot-it-2022.herokuapp.com/",
+    },
+    technologies: [],
+    screenshots: [
+      "https://i.ibb.co/qYSgbHY/Tadmit-3.jpg",
+      "https://i.ibb.co/qYSgbHY/Tadmit-3.jpg",
+    ],
+  },
+];
+
+const technologiesIcons = [
+  {
+    id: 0,
+    alt: "html5",
+    src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg",
+  },
+
+  {
+    id: 1,
+    alt: "css3",
+    src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg",
+  },
+
+  {
+    id: 0,
+    alt: "sass",
+    src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg",
+  },
+  {
+    id: 0,
+    alt: "javascript",
+    src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+  },
+
+  {
+    id: 0,
+    alt: "chartjs",
+    src: "https://www.chartjs.org/media/logo-title.svg",
+  },
+  {
+    id: 0,
+    src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg",
+    alt: "react",
+  },
+  {
+    id: 0,
+    src: "https://reactnative.dev/img/header_logo.svg",
+    alt: "reactnative",
+  },
+
+  {
+    id: 2,
+    alt: "express",
+    src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg",
+  },
+
+  {
+    id: 0,
+    alt: "heroku",
+    src: "https://www.vectorlogo.zone/logos/heroku/heroku-icon.svg",
+  },
+  {
+    id: 0,
+    alt: "nodejs",
+    src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg",
+  },
+
+  {
+    id: 0,
+    alt: "mongodb",
+    src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+  },
+];
+
