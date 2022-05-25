39fd5334e8f241a507698082ad8ed7515ef4ae99
tahrer007 <tahrer_Ad@outlook.com>
2022-05-25 23:45:55 +0300

    side buttons click

--- src/components/project/projectScreenshots/ProjectScreenShots.jsx
+++ src/components/project/projectScreenshots/ProjectScreenShots.jsx
@@ -2,11 +2,13 @@ import React ,{useEffect,useState} from "react";
 import "./projectScreenShots.css";
 
 const ProjectScreenShots = ({ project }) => {
+  const clickRight =()=>console.log("right")
+  const clickLeft =()=>console.log("Left")
   return (
     <div className=" centerFlex screenShotsWraper">
-      <div className="centerFlex sideBtn">left</div>
+      <div className="centerFlex sideBtn" onClick={clickLeft}>left</div>
       <div className="centerFlex mainImage">middle</div>
-      <div className="centerFlex sideBtn">right</div>
+      <div className="centerFlex sideBtn" onClick={clickRight} >right</div>
     </div>
   );
 };
