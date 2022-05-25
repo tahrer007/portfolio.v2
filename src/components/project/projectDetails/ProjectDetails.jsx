2849a75f0c2de04b49354f4b402c66d3e76dd64d
tahrer007 <tahrer_Ad@outlook.com>
2022-05-25 22:36:51 +0300

    refactor component

--- src/components/project/projectDetails/ProjectDetails.jsx
+++ src/components/project/projectDetails/ProjectDetails.jsx
@@ -1,41 +1,31 @@
 import React from "react";
 import "./projectDetails.css";
 
-const ProjectDetails = ({project}) => {
+const ProjectDetails = ({ project }) => {
   const handleLink = (link) => window.open(link);
 
   return (
-    <div className="pages projectsPage">
-      
-          <div
-            className="projectContainer"
-            key={project.id}
-            style={{ flexDirection: project.id % 2 ? "row-reverse" : "row" }}
-          >
-            <div className="projectDetailsBox">
-              <div className="text">
-                <h3>{project.title}</h3>
-                <section>{project.description} </section>
-              </div>
-              <div className="linkes">
-                <button onClick={() => handleLink(project.links.website)}>
-                  View Site
-                </button>
-                <button onClick={() => handleLink(project.links.code)}>
-                  Repo
-                </button>
-              </div>
-              <div className="skillIconsBox">
-                      <img className="skillIcon" src={"https://www.chartjs.org/media/logo-title.svg"} alt="test" />
-
-              </div>
-            </div>
-           
+   
+        <div className="projectDetailsBox">
+          <div className="text">
+            <h3>{project.title}</h3>
+            <section>{project.description} </section>
           </div>
-       
-
-      
-    </div>
+          <div className="linkes">
+            <button onClick={() => handleLink(project.links.website)}>
+              View Site
+            </button>
+            <button onClick={() => handleLink(project.links.code)}>Repo</button>
+          </div>
+          <div className="skillIconsBox">
+            <img
+              className="skillIcon"
+              src={"https://www.chartjs.org/media/logo-title.svg"}
+              alt="test"
+            />
+          </div>
+        </div>
+    
   );
 };
 
--- src/pages/projects/ProjectsPage.jsx
+++ src/pages/projects/ProjectsPage.jsx
@@ -4,34 +4,23 @@ import ProjectDetails from "../../components/project/projectDetails/ProjectDetai
 import ProjectScreenShots from "../../components/project/projectScreenshots/ProjectScreenShots";
 import { technologiesIcons, projectsData } from "../../data/projectsData";
 
- /*<div className="projectContainer" >
+/*<div className="projectContainer" >
             <div className="projectDetailsBox">text</div>
             <div className="projectScreenShoots">iamges</div>
     </div>*/
 
 const ProjectsPage = () => {
- 
   return (
     <div className="pages projectsPage">
       {projectsData.map((project) => {
         return (
-          <div
-            className="projectContainer"
-            key={project.id}
-           ///// style={{ flexDirection: project.id % 2 ? "row-reverse" : "row" }}
-          >
-             <ProjectScreenShots project={project} />
+          <div className="projectContainer" style={{flexDirection:(project.id%2)?"row":"row-reverse"}} >
             <ProjectDetails project={project} />
-          
-         
-           
+            <ProjectScreenShots project={project} />
           </div>
         );
       })}
-
-   
     </div>
-      
   );
 };
 
