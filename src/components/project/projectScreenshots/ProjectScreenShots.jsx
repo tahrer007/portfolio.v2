aa6f89b907e9deb1219eaa703255ab430453f17a
tahrer007 <tahrer_Ad@outlook.com>
2022-05-26 00:31:57 +0300

    main crosela

--- src/components/project/projectScreenshots/ProjectScreenShots.jsx
+++ src/components/project/projectScreenshots/ProjectScreenShots.jsx
@@ -2,12 +2,15 @@ import React ,{useEffect,useState} from "react";
 import "./projectScreenShots.css";
 
 const ProjectScreenShots = ({ project }) => {
-  const clickRight =()=>console.log("right")
-  const clickLeft =()=>console.log("Left")
+  
+  const length =project.screenshots.length
+  const [mainImgIdx,SetMainImgIdx]=useState(0)
+  const clickRight =()=>(mainImgIdx===length-1)?SetMainImgIdx(0):SetMainImgIdx(length-1)
+  const clickLeft =()=>(mainImgIdx===0)?SetMainImgIdx(length-1):SetMainImgIdx(0)
   return (
     <div className=" centerFlex screenShotsWraper">
       <div className="centerFlex sideBtn" onClick={clickLeft}>left</div>
-      <div className="centerFlex mainImage">middle</div>
+      <div className="centerFlex mainImage" style={{backgroundImage : `url(${project?.screenshots[mainImgIdx]})` }}></div>
       <div className="centerFlex sideBtn" onClick={clickRight} >right</div>
     </div>
   );
