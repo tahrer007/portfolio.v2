8f3ea5ba3ca6d187950febec177c5945b4a3c3d2
tahrer007 <tahrer_Ad@outlook.com>
2022-05-25 23:27:11 +0300

    projects skill bar (icons)

--- src/components/project/projectDetails/ProjectDetails.jsx
+++ src/components/project/projectDetails/ProjectDetails.jsx
@@ -1,31 +1,54 @@
-import React from "react";
+import React, { useEffect,useState} from "react";
 import "./projectDetails.css";
+import { technologiesIcons } from "../../../data/projectsData";
 
 const ProjectDetails = ({ project }) => {
   const handleLink = (link) => window.open(link);
+  const skillIcon = (index) => technologiesIcons.find((x) => x.id === index);
+  const [skillsDetails,setSkillsDetails]=useState([]);
+
+  useEffect(() => {
+    //console.log(technologiesIcons);
+    //console.log(skillIcon(2));
+    const arr = project.technologies.map((skill)=>skillIcon(skill));
+    setSkillsDetails(arr);
+
+
+  }, [project?.technologies]);
 
   return (
-   
-        <div className="projectDetailsBox">
-          <div className="text">
-            <h3>{project.title}</h3>
-            <section>{project.description} </section>
-          </div>
-          <div className="linkes">
-            <button onClick={() => handleLink(project.links.website)}>
-              View Site
-            </button>
-            <button onClick={() => handleLink(project.links.code)}>Repo</button>
-          </div>
-          <div className="skillIconsBox">
+    <div className="projectDetailsBox">
+      <div className="text">
+        <h3>{project.title}</h3>
+        <section>{project.description} </section>
+      </div>
+      <div className="linkes">
+        <button
+          className="linkBtn"
+          onClick={() => handleLink(project.links.website)}
+        >
+          View Site
+        </button>
+        <button
+          className="linkBtn"
+          onClick={() => handleLink(project.links.code)}
+        >
+          Repo
+        </button>
+      </div>
+      <div className="skillIconsBox">
+        {skillsDetails.map((skill) => {
+          return (
             <img
+              key={skill.id}
               className="skillIcon"
-              src={"https://www.chartjs.org/media/logo-title.svg"}
-              alt="test"
+              src={skill.src}
+              alt={skill.alt}
             />
-          </div>
-        </div>
-    
+          );
+        })}
+      </div>
+    </div>
   );
 };
 
