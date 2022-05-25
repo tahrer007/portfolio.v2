cef5f1f2ff2f3cdc08afda91f9298a4633345fd5
tahrer007 <tahrer_Ad@outlook.com>
2022-05-25 18:58:59 +0300

    project screen

--- src/pages/projects/ProjectsPage.jsx
+++ src/pages/projects/ProjectsPage.jsx
@@ -1,17 +1,26 @@
 import React from "react";
-import "./projects.css" ;
-import {technologiesIcons,projectsData} from "../../data/projectsData"
+import "./projects.css";
+import { technologiesIcons, projectsData } from "../../data/projectsData";
 
 const ProjectsPage = () => {
-  return <div className="pages projectsPage">
-    <div className="projectContainer">
+  return (
+    <div className="pages projectsPage">
+      {projectsData.map((project) => {
+        return (
+          <div className="projectContainer" key={project.id}style={{ flexDirection: (project.id) ? 'row-reverse' : 'row' }}>
+            <div className="projectDetailsBox">text</div>
+            <div className="projectScreenShoots">iamges</div>
+          </div>
+        );
 
-    <div className="projectDetailsBox">text</div>
-    <div className="projectScreenShoots">iamges</div>
+      })}
+     
+     {/*<div className="projectContainer" >
+            <div className="projectDetailsBox">text</div>
+            <div className="projectScreenShoots">iamges</div>
+    </div>*/}
     </div>
-    
-
-  </div>;
+  );
 };
 
 export default ProjectsPage;
