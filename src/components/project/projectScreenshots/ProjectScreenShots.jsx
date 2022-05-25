64f6a3e3f8805204bdaa15cd81dd73443140ffdd
tahrer007 <tahrer_Ad@outlook.com>
2022-05-26 00:32:48 +0300

    main corsela

--- src/components/project/projectScreenshots/ProjectScreenShots.jsx
+++ src/components/project/projectScreenshots/ProjectScreenShots.jsx
@@ -1,17 +1,25 @@
-import React ,{useEffect,useState} from "react";
+import React, { useEffect, useState } from "react";
 import "./projectScreenShots.css";
 
 const ProjectScreenShots = ({ project }) => {
-  
-  const length =project.screenshots.length
-  const [mainImgIdx,SetMainImgIdx]=useState(0)
-  const clickRight =()=>(mainImgIdx===length-1)?SetMainImgIdx(0):SetMainImgIdx(length-1)
-  const clickLeft =()=>(mainImgIdx===0)?SetMainImgIdx(length-1):SetMainImgIdx(0)
+  const length = project.screenshots.length;
+  const [mainImgIdx, SetMainImgIdx] = useState(0);
+  const clickRight = () =>
+    mainImgIdx === length - 1 ? SetMainImgIdx(0) : SetMainImgIdx(length - 1);
+  const clickLeft = () =>
+    mainImgIdx === 0 ? SetMainImgIdx(length - 1) : SetMainImgIdx(0);
   return (
     <div className=" centerFlex screenShotsWraper">
-      <div className="centerFlex sideBtn" onClick={clickLeft}>left</div>
-      <div className="centerFlex mainImage" style={{backgroundImage : `url(${project?.screenshots[mainImgIdx]})` }}></div>
-      <div className="centerFlex sideBtn" onClick={clickRight} >right</div>
+      <div className="centerFlex sideBtn" onClick={clickLeft}>
+        left
+      </div>
+      <div
+        className="centerFlex mainImage"
+        style={{ backgroundImage: `url(${project?.screenshots[mainImgIdx]})` }}
+      ></div>
+      <div className="centerFlex sideBtn" onClick={clickRight}>
+        right
+      </div>
     </div>
   );
 };
