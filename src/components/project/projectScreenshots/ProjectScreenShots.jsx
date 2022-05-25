998bfc24bd0c6c32f190a3d06b2c31ba743ecc9e
tahrer007 <tahrer_Ad@outlook.com>
2022-05-25 23:43:31 +0300

    .

--- src/components/project/projectScreenshots/ProjectScreenShots.jsx
+++ src/components/project/projectScreenshots/ProjectScreenShots.jsx
@@ -1,17 +1,14 @@
-import React from "react";
+import React ,{useEffect,useState} from "react";
 import "./projectScreenShots.css";
 
-
-
-const ProjectScreenShots = ({project}) => {
- 
-  return <div className=" centerFlex screenShotsWraper">
+const ProjectScreenShots = ({ project }) => {
+  return (
+    <div className=" centerFlex screenShotsWraper">
       <div className="centerFlex sideBtn">left</div>
       <div className="centerFlex mainImage">middle</div>
-      <div className="centerFlex sideBtn" >right</div>
-
-
-  </div>;
+      <div className="centerFlex sideBtn">right</div>
+    </div>
+  );
 };
 
 export default ProjectScreenShots;
