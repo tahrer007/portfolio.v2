756b652e5e2c920356ea478b78753719ed83d114
tahrer007 <tahrer_Ad@outlook.com>
2022-05-26 23:24:38 +0300

    contact icons basic

--- src/pages/contact/ContactPage.jsx
+++ src/pages/contact/ContactPage.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import "./contact.css";
 
 const ContactPage = () => {
-  return <div className="pages">ContactPage</div>;
+  return <div className="pages contactPage">
+    <h1>Feel Free to contact me 😉</h1>
+    <div></div>
+  </div>;
 };
 
 export default ContactPage;
