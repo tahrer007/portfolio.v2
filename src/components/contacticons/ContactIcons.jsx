d409b58ee14cc26841f82ec424fa13a25c46bb1d
tahrer007 <tahrer_Ad@outlook.com>
2022-05-26 23:46:38 +0300

    .

--- src/components/contacticons/ContactIcons.jsx
+++ src/components/contacticons/ContactIcons.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import "./contactIcons.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const ContactIcons = () => {
   const handleLink = (link) => window.open(link);
 
   return (
-    <div className="contactIcons">
-
-
-      test 
-    <FontAwesomeIcon icon="fa-brands fa-github" />    </div>
+    <div className="contactIconsWraper">
+      test
+      <FontAwesomeIcon icon="fa-brands fa-github" />{" "}
+    </div>
   );
 };
 
