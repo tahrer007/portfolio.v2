9fcd51270628c747c97137ddf17848c042f93bef
tahrer007 <tahrer_Ad@outlook.com>
2022-05-26 23:39:16 +0300

    create reusable skill icons component

--- src/pages/contact/ContactPage.jsx
+++ src/pages/contact/ContactPage.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./contact.css";
+import ContactIcons from "../../components/contacticons/ContactIcons";
 
 const ContactPage = () => {
   return <div className="pages contactPage">
     <h1>Feel Free to contact me 😉</h1>
-    <div></div>
+        <ContactIcons/>
   </div>;
 };
 
