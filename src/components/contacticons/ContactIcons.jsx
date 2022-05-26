a5b5fd3490aab4166859ebe730dc3a152b47561b
tahrer007 <tahrer_Ad@outlook.com>
2022-05-26 23:24:47 +0300

    contact icons basics

---
+++ src/components/contacticons/ContactIcons.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./contactIcons.css";
+const ContactIcons = () => {
+  const handleLink = (link) => window.open(link);
+
+  return (
+    <div className="contactIcons">
+       contact icons  
+    </div>
+  );
+};
+
+export default ContactIcons;
