a109fe72f73c74f7ec46ecdfbc738520d8a7dbf4
tahrer007 <tahrer_Ad@outlook.com>
2022-05-26 22:57:07 +0300

    update about me

--- src/components/personalDetails/PersonalDetails.jsx
+++ src/components/personalDetails/PersonalDetails.jsx
@@ -3,10 +3,20 @@ import "./personalDetails.css";
 import { aboutMe } from "../../data/AboutPageData";
 
 const PersonalDetails = () => {
+  const handleLink = (link) => window.open(link);
+
   return (
     <div className="personalDetails">
       <div className="profilePic"></div>
+
       <div className="pDetails">{aboutMe}</div>
+      <div>
+      <button className="linkBtn" onClick={() => handleLink("test!!!")}>
+        Download Resume
+      </button>
+      icons   
+
+      </div>
     </div>
   );
 };
