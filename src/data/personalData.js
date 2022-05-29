6ebe41cec582c0101ca8d959bb6113892497ef49
tahrer007 <tahrer_Ad@outlook.com>
2022-05-29 13:57:15 +0300

    skills text

--- src/data/personalData.js
+++ src/data/personalData.js
@@ -1,68 +1,29 @@
 const skillsDetails = [
   {
     title: "",
-    details: "not giveup till get to the target",
-    url: "https://cdn-icons-png.flaticon.com/512/2755/2755538.png",
+    details: "not give up till win",
+    url: "https://cdn-icons-png.flaticon.com/512/4727/4727555.png",
   },
   {
     title: "",
     details: "Good team player",
-    url: "https://cdn-icons-png.flaticon.com/512/2755/2755492.png",
+    url: "https://cdn-icons-png.flaticon.com/512/4727/4727533.png",
   },
   {
     title: "",
     details: "extraordenary problem solving skills",
-    url: "https://cdn-icons-png.flaticon.com/512/2755/2755348.png",
+    url: "https://cdn-icons-png.flaticon.com/512/4727/4727443.png",
   },
   {
     title: "",
-    details: "high ability to selfstudy",
-    url: "https://cdn-icons-png.flaticon.com/512/2755/2755387.png",
+    details: "Learning and working against the clock",
+    url: "https://cdn-icons-png.flaticon.com/512/4727/4727565.png",
   },
 ];
-const skillsLabels = [
-  "HTML",
-  "CSS",
-  "JavaScript",
-  "React",
-  "React Native",
-  "Node.js",
-  "MongooDB",
-];
-const skillsPercentages = [
-  {
-    name :"HTML",
-    percentage : 85
-  },
-  {
-    name :"CSS",
-    percentage : 90
-  },
-  {
-    name :"JavaScript",
-    percentage : 85
-  },
-  {
-    name :"React",
-    percentage : 85
-  },
-  {
-    name :"ReactNative",
-    percentage : 70
-  },
-  {
-    name :"Node.js",
-    percentage : 70
-  },
-  {
-    name :"MongooDB",
-    percentage : 75
-  },
-  
-  
-];
-const resumeLink= "https://drive.google.com/file/d/1vx50mpesNH79lXnLoKQRtM_ZN8C7GChv/view"
+
+const resumeLink =
+  "https://drive.google.com/file/d/1vx50mpesNH79lXnLoKQRtM_ZN8C7GChv/view";
 
 const aboutMe =
   "Hi, my name is Tahrer and I’m a Full-Stack Developer, with a great passion for coding and developing new practical websites.";
-export { skillsDetails, aboutMe, skillsLabels ,skillsPercentages ,resumeLink};
+export { skillsDetails, aboutMe, resumeLink };
