05cf9cd650da6bdecf0c1b748a885dd238f45b94
tahrer007 <tahrer_Ad@outlook.com>
2022-05-30 04:38:56 +0300

    form styling

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./form.scss";
-
 
 const Form = () => {
   const handleLink = (link) => window.open(link);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setmessage] = useState("");
+  const handelSubmit =(e)=>{
+
+    console.log(e)
+      e.preventDefault(); 
+  }
 
   return (
-    <div className="form">
-      form
+    <div className="formWraper">
+        <h3>Lets get in touch</h3>
+      <form onSubmit={handelSubmit}>
+        <label>
+          Name:
+          <input type="text" name="name" onChange={(text)=>setName(text)} value={name}/>
+        </label>
+
+        <label>
+          Email:
+          <input type="email" name="email" onChange={(text)=>setEmail(text)} value={email} />
+        </label>
+
+        <label>
+          Message:
+          <textarea value={message} onChange={(text)=>setmessage(text)} rows="6" cols="50" />
+        </label>
+        <input type="submit" value="Submit" className="btn"/>
+      </form>
     </div>
   );
 };
