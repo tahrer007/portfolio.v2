4f5b3ed7eb5b2ff5dbb2dd201d5131f61bfadee9
tahrer007 <tahrer_Ad@outlook.com>
2022-05-31 20:27:58 +0300

    add ityped to contact mesage

--- src/pages/contact/ContactPage.jsx
+++ src/pages/contact/ContactPage.jsx
@@ -1,8 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./contact.scss";
 import Form from "../../components/form/Form";
+import { init } from "ityped";
+
 const ContactPage = () => {
   const [success, setSuccess] = useState(false);
+
+  const text1Ref = useRef();
+  const text2Ref = useRef();
+
+  useEffect(() => {
+    if (!success) return;
+
+    init(text1Ref.current, {
+      showCursor: false,
+      //backDelay: 3000,
+      typeSpeed: 100,
+      disableBackTyping: true,
+
+      strings: ["Thank you "],
+    });
+    init(text2Ref.current, {
+      showCursor: false,
+      typeSpeed: 100,
+      startDelay: 1500,
+      disableBackTyping: true,
+
+      strings: ["for getting in touch!"],
+    });
+  }, [success]);
   const successed = () => {
     console.log("succes!!! ");
 
@@ -10,13 +36,15 @@ const ContactPage = () => {
   };
 
   return (
-    <section id="contact" className="contactPage" >
-      
-
+    <section id="contact" className="contactPage">
       <div className="img"></div>
       {success ? (
         <div className="success">
-          <h2> <span className="greenFont"> Thank you </span>for getting in touch!</h2>
+          <h2>
+            {" "}
+            <span className="greenFont" ref={text1Ref}></span>
+            <span ref={text2Ref}></span>
+          </h2>
         </div>
       ) : (
         <Form successed={successed} />
