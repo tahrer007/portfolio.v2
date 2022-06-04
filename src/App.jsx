97ca8d25744b360fa5466733c166a9479af54a95
tahrer007 <tahrer_Ad@outlook.com>
2022-06-05 00:57:14 +0300

    scroll to the top code

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,22 @@ import TopBar from "./components/topBar/TopBar";
 import SideNav from "./components/sideNav/SideNav";
 import "./app.scss";
 import { Routes, Route } from "react-router-dom";
+import ScrollToTop from "./services/scrolltotop";
+ 
 
 const App = () => {
   return (
+    
     <div className="app">
+      {/*TODO:fix scroll to the top !!  */}
+     {/*<ScrollToTop/>*/}
       <TopBar />
+
+    
       <SideNav />
 
       <div className="sections">
-        {/*<HomePage />
-        <ProjectsPage />
-        <AboutPage />
-      <ContactPage />*/}
-
+      
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="about" element={<AboutPage />} />
