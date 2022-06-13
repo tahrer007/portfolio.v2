674c98b44d3f5243a24cb1f3ce163bbe11645b1d
tahrer007 <tahrer_Ad@outlook.com>
2022-06-13 17:54:49 +0300

    nav tag

--- src/components/sideNav/sideNav.jsx
+++ src/components/sideNav/sideNav.jsx
@@ -6,7 +6,7 @@ import "./sideNav.scss";
 const SideNav = () => {
   useEffect(() => {}, []);
   return (
-    <div className="sideNav">
+    <nav className="sideNav">
       <Link to="home" className="linksSideBar" spy={true} smooth={true} offset={50} duration={400}>
         <svg
           className="svgIcons"
@@ -67,7 +67,7 @@ const SideNav = () => {
           <path d="M511.1 63.1v287.1c0 35.25-28.75 63.1-64 63.1h-144l-124.9 93.68c-7.875 5.75-19.12 .0497-19.12-9.7v-83.98h-96c-35.25 0-64-28.75-64-63.1V63.1c0-35.25 28.75-63.1 64-63.1h384C483.2 0 511.1 28.75 511.1 63.1z" />
         </svg>
       </Link>
-    </div>
+    </nav>
   );
 };
 
